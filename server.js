4537a310c929b30989a16f43a598477b41be3b4a
Nathan N <Nitharios@users.noreply.github.com>
2017-10-18 16:55:42 -1000

    Organized functions

--- server.js
+++ server.js
@@ -23,6 +23,28 @@ app.get('/gallery', (req, res) => {
     .then(galleryInformation => {
       return res.json(galleryInformation);
     });
+  })
+  //POST/gallery : create a new gallery photo
+  .post('/gallery', (req, res) => {
+    //{author: string, link: string, description: string}
+    const author = req.body.author;
+    const link = req.body.link;
+    const description = req.body.description;
+
+    return Gallery.create({
+      author : author,
+      link : link,
+      description : description
+    })
+      .then(newPicture => {
+        console.log('POST is done');
+        return res.json(newPicture);
+      });
+  });
+
+//GET/gallery/new to see a 'new photo' form
+app.get('/gallery/new', (req, res) => {
+  return res.render('/views/partials/new');
 });
 
 //GET/gallery/:id to see a single photo
@@ -32,12 +54,47 @@ app.get('/gallery/:id', (req, res) => {
     .then(pictureInformation => {
       return res.json(pictureInformation);
     });
-});
+  })
+//]PUT/gallery/:id : updates a single gallery photo identified by :id
+  .put('/gallery/:id', (req, res) => {
+    const id = req.params.id;
+    const data = req.body; 
+    //{author: string, link: string, description: string}
 
-//GET/gallery/new to see a 'new photo' form
-app.get('/gallery/new', (req, res) => {
-  return res.render('/views/partials/new');
-});
+    return Gallery.findById(id)
+      .then(pictureInformation => {
+        let updateObject = {};
+
+        if (data.author) Gallery.update({ 
+          author : data.author }, {
+            where : { id : id }
+          });
+        if (data.link) Gallery.update({ 
+          link : data.link }, {
+            where : { id : id }
+          });
+        if (data.description) Gallery.update({ description : data.description}, {
+            where : { id : id }
+          });
+
+        console.log('Picture edited');
+        return res.json(pictureInformation);
+      });
+  })
+  //DELETE/gallery/:id : deletes a single gallery photo ientified by :id
+  .delete('/gallery/:id', (req, res) => {
+    const id = req.params.id;
+
+    return Gallery.findById(id)
+      .then((pictureInformation) => {
+        Gallery.destroy({ where : {
+          id : id
+        }});
+
+        console.log('Picture deleted');
+        return res.json(pictureInformation);
+      });
+  });
 
 //GET/gallery/:id/edit : see a form to edit a gallery identified by :id
 //fields are: author : text, link : Text(image URL), descrip : TextArea
@@ -46,66 +103,6 @@ app.get('/gallery/:id/edit', (req, res) => {
   return Gallery.findById(id)
     .then(pictureInformation => {
       return res.render('partials/edit', { pictureInformation });
-    });
-});
-
-//POST/gallery : create a new gallery photo i
-app.post('/gallery', (req, res) => {
-  //{author: string, link: string, description: string}
-  const author = req.body.author;
-  const link = req.body.link;
-  const description = req.body.description;
-
-  return Gallery.create({
-    author : author,
-    link : link,
-    description : description
-  })
-    .then(newPicture => {
-      console.log('POST is done');
-      return res.json(newPicture);
-    });
-});
-
-//PUT/gallery/:id : updates a single gallery photo identified by :id
-app.put('/gallery/:id', (req, res) => {
-  const id = req.params.id;
-  const data = req.body; 
-  //{author: string, link: string, description: string}
-
-  return Gallery.findById(id)
-    .then(pictureInformation => {
-      let updateObject = {};
-
-      if (data.author) Gallery.update({ 
-        author : data.author }, {
-          where : { id : id }
-        });
-      if (data.link) Gallery.update({ 
-        link : data.link }, {
-          where : { id : id }
-        });
-      if (data.description) Gallery.update({ description : data.description}, {
-          where : { id : id }
-        });
-
-      console.log('Picture edited');
-      return res.json(pictureInformation);
-    });
-});
-
-//DELETE/gallery/:id : deletes a single gallery photo ientified by :id
-app.delete('/gallery/:id', (req, res) => {
-  const id = req.params.id;
-
-  return Gallery.findById(id)
-    .then((pictureInformation) => {
-      Gallery.destroy({ where : {
-        id : id
-      }});
-
-      console.log('Picture deleted');
-      return res.json(pictureInformation);
     });
 });
 
