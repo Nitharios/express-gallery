d951ee77cdee3220536ffac26a1eb31f13c27528
Chris Min <chris.min216@gmail.com>
2017-10-18 19:18:13 -1000

    HBS pages done

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 /* jshint esversion:6 */
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const handlebars= require('express-handlebars');
 const methodOverride = require('method-override');
-
 const PORT = process.env.PORT || 8080; //change?
+
 const db = require('./models');
 const Gallery = db.gallery;
 const Author = db.author;
@@ -23,16 +22,14 @@ app.get('/', (req, res) => {
   return res.render('index');
 });
 
-//GET to view a list of gallery photos
 app.get('/gallery', (req, res) => {
   return Gallery.findAll()
     .then(galleryInformation => {
       return res.render('/partials/gallery', galleryInformation);
     });
   })
-  //POST/gallery : create a new gallery photo
   .post('/gallery', (req, res) => {
-    //{author: string, link: string, description: string}
+    /*{author: string, link: string, description: string}*/
     const author = req.body.author;
     const link = req.body.link;
     const description = req.body.description;
@@ -47,12 +44,10 @@ app.get('/gallery', (req, res) => {
       });
   });
 
-//GET/gallery/new to see a 'new photo' form
 app.get('/gallery/new', (req, res) => {
   return res.render('/partials/new');
 });
 
-//GET/gallery/:id to see a single photo
 app.get('/gallery/:id', (req, res) => {
   const id = req.params.id;
   return Gallery.findById(id) 
@@ -60,16 +55,15 @@ app.get('/gallery/:id', (req, res) => {
       return res.render('/partials/gallery_single', pictureInformation);
     });
   })
-//]PUT/gallery/:id : updates a single gallery photo identified by :id
   .put('/gallery/:id', (req, res) => {
     const id = req.params.id;
     const data = req.body; 
-    //{author: string, link: string, description: string}
+    /*{author: string, link: string, description: string}*/
 
     return Gallery.findById(id)
       .then(pictureInformation => {
         let updateObject = {};
-        //change this to be able to handle author-gallery association
+        //how to handle author-gallery association in model?
         if (data.author) Gallery.update({ 
           author : data.author }, {
             where : { id : id }
@@ -82,32 +76,25 @@ app.get('/gallery/:id', (req, res) => {
             where : { id : id }
           });
 
-        console.log('Picture edited');
-        return res.render('/partials/gallery_single', pictureInformation);
+        return res.render("/partials/gallery_single", pictureInformation);
       });
   })
-  //DELETE/gallery/:id : deletes a single gallery photo ientified by :id
   .delete('/gallery/:id', (req, res) => {
     const id = req.params.id;
-
     return Gallery.findById(id)
       .then((pictureInformation) => {
         Gallery.destroy({ where : {
           id : id
         }});
-
-        console.log('Picture deleted');
         return res.render('/partials/gallery');
       });
   });
 
-//GET/gallery/:id/edit : see a form to edit a gallery identified by :id
-//fields are: author : text, link : Text(image URL), descrip : TextArea
 app.get('/gallery/:id/edit', (req, res) => {
   const id = req.params.id;
   return Gallery.findById(id)
     .then(pictureInformation => {
-      return res.render('/partials/edit', { pictureInformation });
+      return res.render('/partials/edit', pictureInformation);
     });
 });
 
