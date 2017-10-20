e2b16a17dd2c13935d8c448d0549883667e0dcd4
Nathan N <Nitharios@users.noreply.github.com>
2017-10-19 15:04:15 -1000

    Basic user authentication setup

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
 /* jshint esversion:6 */
 module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define('user', {
-    user : DataTypes.STRING,
+    username : { type: DataTypes.STRING, unique : true },
+    password : DataTypes.STRING
   }, {
     tableName : 'users'
   });
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 /* jshint esversion:6 */
 const express = require('express');
 const bodyParser = require('body-parser');
-const handlebars= require('express-handlebars');
+const handlebars = require('express-handlebars');
 const methodOverride = require('method-override');
 const PORT = process.env.PORT || 8080; //change?
 
 const bcrypt = require('bcrypt');
+const passport = require('passport');
 const session = require('express-session');
 const LocalStrategy = require('passport-local').Strategy;
 const saltRounds = 12;
@@ -16,10 +17,14 @@ const User = db.user;
 
 const app = express();
 
-app.engine('hbs', handlebars({defaultLayout: 'main', extname: '.hbs'}));
+app.engine('hbs', handlebars({ 
+  defaultLayout : 'main', 
+  extname: '.hbs' 
+}));
+
 app.set('view engine', 'hbs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 app.use(session({
@@ -27,54 +32,103 @@ app.use(session({
   resave: false,
   saveInitialized: false
 }));
+
 app.use(express.static('public'));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 /*AUTHENTICATION*/
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { next(); }
+  else { res.redirect('/'); }
+}
+
 passport.serializeUser((user, done) => {
-  console.log('Serializing');
+  console.log('serializing');
   return done(null, {
-    id: user.id,
-    username: user.username
+    id : user.id,
+    username : user.username
   });
 });
 
 passport.deserializeUser((user, done) => {
-  console.log('Deserializing');
-  db.users.findOne({ where : { id : user.id}})
+  console.log('deserializing');
+  db.user.findOne({ where : { id : user.id} })
     .then(user => {
-      return done(null, user);
+      return done(null, {
+        id : user.id,
+        username : user.username
+      });
     });
 });
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    db.users.findOne({ where : {username : username} })
-      .then(user => {
-        if (user === null) {
-          return done(null, false, {message: 'bad username or password'});
-        } else {
-          bcrypt.compare(password, user.password)
-            .then(res => {
-              console.log(res);
-              if (res) { return done(null, user); }
-              else {
-                return done(null, false, {message: 'bad username or password'});
-              }
-            })
-        }
-      })
-      .catch(err => {
-
-      });
-}
+passport.use(new LocalStrategy(function(username, password, done) {
+  db.user.findOne({ where : { username : username }})
+    .then(user => {
+      if (user === null) {
+        return done(null, false, { message : 'bad username or password' });
+      }
+      else {
+        bcrypt.compare(password, user.password)
+          .then(res => {
+            console.log(res);
+            // res 'basically' tells you TRUE or FALSE
+            if (res) { return done(null, user); }
+            else {
+              return done(null, false, { message : 'bad username or password'});
+            }
+          });
+      }
+    })
+    .catch(err => { console.log('error : ', err); });
+}));
 
 /*ROUTES*/
 app.get('/', (req, res) => {
-  return res.render('index');
+  return res.render('index', { home : true });
 });
+
+app.get('/login', (req, res) => {
+  return res.render('partials/login');
+})
+  .post('/login', passport.authenticate('local', {
+    successRedirect : '/secret',
+    failureRedirect : '/'
+  }));
+
+app.post('/logout', (req, res) => {
+  req.logout();
+  res.sendStatus(200);
+  res.redirect('/gallery');
+});
+
+app.get('/secret', isAuthenticated, (req, res) => {
+  console.log('req.user: ', req.user);
+  console.log('req.user.id: ', req.user.id);
+  console.log('req.username: ', req.user.username);
+  console.log('req.user.password: ', req.user.password);
+  res.send('you found the secret!');
+});
+
+app.get('/register', (req, res) => {
+  return res.render('partials/register');
+})
+  .post('/register', (req, res) => {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      bcrypt.hash(req.body.password, salt, function(err, hash) {
+        db.user.create({
+          username : req.body.username,
+          password : hash
+        })
+        .then(user => {
+          console.log(user);
+          res.redirect('/');
+        })
+        .catch(err => { return res.send('stupid username'); });
+      });
+    });
+  });
 
 app.get('/gallery', (req, res) => {
   return Gallery.findAll()
@@ -86,13 +140,13 @@ app.get('/gallery', (req, res) => {
     });
   })
   .post('/gallery', (req, res) => {
-    /*{author: string, link: string, description: string}*/
-    const author = req.body.author;
+    /*{user: string, link: string, description: string}*/
+    const user = req.body.user;
     const link = req.body.link;
     const description = req.body.description;
 
     return Gallery.create({
-      author : author,
+      user : user,
       link : link,
       description : description
     })
@@ -118,13 +172,13 @@ app.get('/gallery/:id', (req, res) => {
   .put('/gallery/:id', (req, res) => {
     const id = req.params.id;
     const data = req.body; 
-    /*{author: string, link: string, description: string}*/
+    /*{user: string, link: string, description: string}*/
     return Gallery.findById(id)
       .then(pictureInformation => {
         let updateObject = {};
-        //how to handle author-gallery association in model?
-        if (data.author) Gallery.update({ 
-          author : data.author }, {
+        //how to handle user-gallery association in model?
+        if (data.user) Gallery.update({ 
+          user : data.user }, {
             where : { id : id }
           });
         if (data.link) Gallery.update({ 
@@ -163,6 +217,6 @@ app.get('/gallery/:id/edit', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  db.sequelize.sync({ force: false });
+  db.sequelize.sync({ force: true });
   console.log('Server running on ' + PORT);
 });
